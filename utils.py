f03d99dee0e24479b2dd3bfdcdca76ebc2a0b63e
Kevin Q Li <likevinq@gmail.com>
2018-02-16 16:47:19 -0800

    display size by author

--- utils.py
+++ utils.py
@@ -10,10 +10,24 @@ def organize_by_author():
     for f in files:
         if f[0] != '.' and os.path.splitext(f)[1] == '.txt':
             author = f[:f.find('_')]
+            author_dir = os.path.join(DATA_DIR, author)
             if author not in authors:
                 authors.add(author)
-                if not os.path.exists(os.path.join(DATA_DIR, author)):
-                    os.mkdir(os.path.join(DATA_DIR, author))
-                print author
+                if not os.path.exists(author_dir):
+                    os.mkdir(author_dir)
             os.rename(os.path.join(DATA_DIR, f),
-                      os.path.join(DATA_DIR, author, f))
+                      os.path.join(author_dir, f))
+
+# data should already be organized using organize_by_author
+def list_author_sizes():
+    sizes = []
+    for author in os.listdir(DATA_DIR):
+        if author[0] != '.':
+            author_dir = os.path.join(DATA_DIR, author)
+            size = sum(os.path.getsize(os.path.join(author_dir, f))
+                                       for f in os.listdir(author_dir) if
+                                       os.path.isfile(os.path.join(author_dir,
+                                                                   f)))
+            print('%s: %d' % (author, size))
+            sizes.append((author, size))
+    print(sorted(sizes, key=lambda x: x[1]))
