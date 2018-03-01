01d66500dddf1501e7257385596b899ef0560bdd
Kevin Q Li <likevinq@gmail.com>
2018-02-28 18:54:38 -0800

    punctuation sample creation

---
+++ scripts/create_punc_samples.py
@@ -0,0 +1,124 @@
+import os
+from os.path import join, isdir
+import cPickle as pickle
+import random
+
+'''
+This script is designed for one-time use.
+It will take the directories of authors, with files of books inside,
+and reorganize it into one file of all the author's text.
+
+After trimming, we strip out all alphanumeric characters, leaving
+punctuation only. Then the file is split into consecutive chunks of 500
+punctuations, and distributed into train/val/test with ratio 80/10/10 resp.
+'''
+
+DATA_DIR = '../Gutenberg' # change this to be actual dir
+OUTPUT_DIR = '../data'
+AUTHOR_LIST_FILE = 'unique_authors.txt'
+TRAIN_DIR = join(OUTPUT_DIR, 'train')
+VAL_DIR = join(OUTPUT_DIR, 'val')
+TEST_DIR = join(OUTPUT_DIR, 'test')
+PUNCS = set('.,?!\'":;-()')
+
+
+TRIM_MARGIN = 100
+THRESHOLD = 2000
+CHUNK_LENGTH = 500
+
+def get_author_dirs(path=DATA_DIR):
+    print('Getting list of authors from: %s' % AUTHOR_LIST_FILE)
+    with open(AUTHOR_LIST_FILE) as f:
+        author_list = set(map(str.strip, f.read().splitlines()))
+
+    print('Retrieving the authors under: %s' % path)
+    alist = [d for d in os.listdir(path) if d in author_list]
+    print('We found authors: %s' % str(alist))
+    return alist
+
+def strip_to_puncs(s):
+    import re
+    tokens = re.findall(r'[\w\s]*[\w]|[\.,\?!\'":;-]|[(]|[)]', s)
+    tokens = [len(t.split()) if t not in PUNCS else t for t in tokens]
+    tokens = [((tokens[i-1], tokens[i]) if i >= 1 and tokens[i-1] not in PUNCS
+              else (0, tokens[i])) for i in range(len(tokens)) if tokens[i] in
+                                                 PUNCS]
+    return tokens
+
+def get_puncs_in_dir(adir):
+    puncs = [] # list of punctuations from all texts under the author
+
+    print('Getting puncutations from author %s.' % adir)
+    author_files = [f for f in os.listdir(join(DATA_DIR, adir)) if f[-4:] ==
+                    '.txt' and f[0] != '.']
+    print('Found texts: %s' % author_files)
+
+    for fil in author_files:
+        with open(join(DATA_DIR, adir, fil), 'r') as f:
+            contents = strip_to_puncs(f.read())
+
+        print('length of content (# puncs) for %s, %d.' % (fil, len(contents)))
+        if len(contents) < THRESHOLD:
+            print('File %s has < %d puncuations. Skipping.' % (fil, THRESHOLD))
+
+        # Cut to multiple of CHUNK_LENGTH.
+        # Don't want a chunk bridging two documents later
+        truncate_length = (len(contents) // CHUNK_LENGTH) * CHUNK_LENGTH
+        contents = contents[:truncate_length]
+        puncs.extend(contents)
+
+    print('Aggregated text has %d puncs (author %s).' % (len(puncs), adir))
+    return puncs
+
+def distribute_into_output_dir(authorname, puncs):
+    assert(len(puncs) % CHUNK_LENGTH == 0)
+    print('Distributing chunks into dir for %s' % authorname)
+
+    for split in [TRAIN_DIR, VAL_DIR, TEST_DIR]:
+        if not isdir(join(split, authorname)):
+            print('Made new dir for %s, split %s' % (authorname, split))
+            os.makedirs(join(split, authorname))
+
+    index = 0
+    chop = 0
+    while chop < len(puncs):
+
+        # access a chunk
+        chunk = puncs[chop:chop+CHUNK_LENGTH]
+        payload = ' '.join(chunk)
+        chop += CHUNK_LENGTH
+
+        # Determine if it goes into train, dev, test
+        diceroll = random.random()
+        split_dir = TRAIN_DIR if diceroll < 0.8 else (VAL_DIR if diceroll < 0.9 else TEST_DIR)
+
+        chunk_name = '%s__%010d.txt' % (authorname, index) # have author, and index padded up to 10 places
+
+        destination = join(split_dir, authorname, chunk_name)
+
+        # put in destination under author with that name
+        with open(destination, 'w') as f:
+            pickle.dump(payload, f)
+
+        index += 1
+        if index % 100 == 0:
+            print('At index %d...' % index)
+
+    print('Done. total %d chunks (of 150 words) created.' % index)
+
+if __name__ == '__main__':
+    author_dirs = get_author_dirs()
+    existing_adirs = get_author_dirs(path=TEST_DIR) # any split works (except train, removed from those for space)
+
+    remaining_adirs = set(author_dirs) - set(existing_adirs)
+    print('The remaining authors are: %s' % str(remaining_adirs))
+    errored_authors = []
+
+    for adir in remaining_adirs: # adir is the author name string
+        try:
+            puncs = get_puncs_in_dir(adir)
+            distribute_into_output_dir(adir, puncs)
+        except Exception as e:
+            print('Got error: %s' % str(e))
+            print('Author %s may not be finished!' % adir)
+            errored_authors.append(adir)
