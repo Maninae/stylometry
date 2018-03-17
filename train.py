637bb36193be8b0ace334f4864295ef9dce35d19
Owen Wang <ojwang@stanford.edu>
2018-03-17 01:43:33 -0700

    Working on train.py. ModelOperations class

--- train.py
+++ train.py
@@ -7,24 +7,27 @@ class Config(object):
 
 config = Config()
 
-def train(model):
-    X_train, Y_train = load_data('train')
-    X_val, Y_val = load_data('val')
-    model.compile(loss='categorial_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
-    model.fit(x=X_train,
-              y=Y_train,
-              batch_size=config.batch_size,
-              epochs=config.epochs,
-              verbose=1,
-              validation_data=(X_val, Y_val),
-              shuffle=True)
+class ModelOperations(object):
+    self.config = Config()
+
+    def train(self, model):
+        X_train, Y_train = load_data('train')
+        X_val, Y_val = load_data('val')
+        model.compile(loss='categorial_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+        model.fit(x=X_train,
+                  y=Y_train,
+                  batch_size=config.batch_size,
+                  epochs=config.epochs,
+                  verbose=1,
+                  validation_data=(X_val, Y_val),
+                  shuffle=True)
 
 
-def test(model):
-    X_test, Y_test = load_data('test')
-    model.evaluate(x=X_test,
-                   y=Y_test,
-                   batch_size=config.batch_size,
-                   verbose=1)
+    def test(self, model):
+        X_test, Y_test = load_data('test')
+        model.evaluate(x=X_test,
+                       y=Y_test,
+                       batch_size=config.batch_size,
+                       verbose=1)
